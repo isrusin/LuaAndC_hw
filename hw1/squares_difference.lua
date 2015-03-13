89cbe8b9729dfece3997653f5ee024bf24b0e410
Ivan Rusinov <is_rusinov@mail.ru>
2015-03-13 09:01:53 +0300

    correction of squares_difference (return only integers)

--- hw1/squares_difference.lua
+++ hw1/squares_difference.lua
@@ -1,13 +1,21 @@
-require("math")
-
 local num
-repeat
-	io.write("enter natural number: ")
+while true do
+	io.write("enter a natural number: ")
 	num = tonumber(io.read())
-until num
+	if num and num % 1 == 0 and num > 0 then
+		break
+	end
+end
 
-local a = math.abs(num + 1) / 2
-local b = math.abs(num - 1) / 2
-
-print(a, b)
+if num % 2 ~= 0 then
+	local a = (num + 1) / 2
+	local b = (num - 1) / 2
+	print(a, b)
+elseif num % 4 ~= 0 or num == 4 then
+	print(num .. " can't be presented as a difference of integers.")
+else
+	local a = num / 4 + 1
+	local b = num / 4 - 1
+	print(a, b)
+end
 
